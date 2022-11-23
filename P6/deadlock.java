0b3b997dd241a938fe3bc9c2d0d2622838f0d410
manupruiz <rayerito23@gmail.com>
2022-11-24 00:53:46 +0100

    P6 terminada

--- P6/deadlock.java
+++ P6/deadlock.java
@@ -1,5 +1,100 @@
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
 
+/**
+ * Ejercicio 4 - Práctica 6
+ * @author Manuel Pérez Ruiz
+ */
 
+ /**
+  * Clase deadlock que crea un deadlock entre tres hebras.
+  */
 public class deadlock extends Thread{
+    /**
+     * Objeto lock1 de la clase Object que usaremos como cerrojo
+     */
+    private static Object lock1 = new Object();
+    /**
+     * Objeto lock2 de la clase Object que usaremos como cerrojo
+     */
+    private static Object lock2 = new Object();
+    /**
+     * Objeto lock3 de la clase Object que usaremos como cerrojo
+     */
+    private static Object lock3 = new Object();
+    /**
+     * Variable que usaremos para saber a qué hebra recurre
+     */
+    private int aux;
+
+    /**
+     * Constructor de la clase deadlock
+     * @param aux Variable que usaremos para saber a qué hebra recurre
+     */
+    public deadlock(int aux){
+        this.aux = aux;
+    }
+
+    /**
+     * Método run de la clase deadlock. Crea un deadlock entre las tres hebras que hemos creado.
+     */
+    public void run(){
+        switch(aux){
+            case 0:
+                synchronized(lock1){
+                    synchronized(lock2){
+                        synchronized(lock3){
+                            System.out.println("Hilo " + this.getName() + " bloqueado con lock3");
+                        }
+                    }
+                }
+            break;
+
+            case 1:
+                synchronized(lock3){
+                    synchronized(lock2){
+                        synchronized(lock1){
+                            System.out.println("Hilo " + this.getName() + " bloqueado con lock1");
+                        }
+                    }
+                }
+            break;
+
+            case 2:
+                synchronized(lock1){
+                    synchronized(lock3){
+                        synchronized(lock2){
+                            System.out.println("Hilo " + this.getName() + " bloqueado con lock2");
+                        }
+                    }
+                }
+            break;
+
+            default:
+                System.out.println("Error...");
+            break;
+        }
+    }
     
+    /**
+     * Método main de la clase deadlock.
+     * @param args
+     */
+    public static void main(String[] args) {
+        
+        ThreadPoolExecutor pool = new ThreadPoolExecutor(6, 12, 0L, TimeUnit.MILLISECONDS, new LinkedBlockingQueue<Runnable>());
+
+        for(int i = 0; i < 100; i++){
+            pool.execute(new deadlock((int)(Math.random()*3)));
+        }
+        pool.shutdown();
+        while(!pool.isTerminated()){}
+        
+    }
+    
+    
+
+
 }
+
---
+++ P6/integCallable.java
@@ -0,0 +1,86 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Ejercicio 5 - Práctica 6
+ * @author Manuel Pérez Ruiz
+ */
+
+ // Versión de la integración paralela de Monte Carlo para aproximar la integral definida en la función f(x) = cos(x) en [0,1] utilizando tareas Callable y la interfaz Future.
+public class integCallable implements Callable<Double>{
+
+    /**
+     * Atributos de la clase integCallable
+     * @param inicio inicio del rango
+     * @param fin fin del rango
+     * @param n número de puntos
+     */
+    public double inicio;
+    public double fin;
+    public int n;
+
+    /**
+     * Constructor de la clase integCallable
+     * @param inicio inicio del rango
+     * @param fin fin del rango
+     * @param n número de puntos
+     */
+    public integCallable(double inicio, double fin, int n) {
+        this.inicio = inicio;
+        this.fin = fin;
+        this.n = n;
+    }
+
+    /**
+     * Método call de la clase integCallable
+     */
+    public Double call() {
+        double x, y, area, suma = 0;
+        for (int i = 0; i < n; i++) {
+            x = Math.random() * (fin - inicio) + inicio;
+            y = Math.random();
+            if (y <= Math.cos(x)) {
+                suma++;
+            }
+        }
+        area = (fin - inicio) * suma / n;
+        return area;
+    }
+    
+    public static void main(String[] args) {
+        int n = 100000;
+        int nTareas = 100;
+        double inicio = 0;
+        double fin = 1;
+        double area = 0;
+        double paso = (fin - inicio) / nTareas;
+        ThreadPoolExecutor pool = new ThreadPoolExecutor(6,12,0L,TimeUnit.MILLISECONDS,new LinkedBlockingQueue<>());
+        
+        List<Future<Double>> lista = new ArrayList<Future<Double>>();
+        for (int i = 0; i < nTareas; i++) {
+            Callable<Double> tarea = new integCallable(inicio + i * paso, inicio + (i + 1) * paso, n);
+            Future<Double> future = pool.submit(tarea);
+            lista.add(future);
+        }
+
+
+        for (Future<Double> future : lista) {
+            try {
+                area += future.get();
+            } catch (InterruptedException | ExecutionException e) {
+                e.printStackTrace();
+            }
+        }
+        pool.shutdown();
+        System.out.println("El área es: " + area);
+    }
+    
+}
